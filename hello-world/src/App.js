42851e0d39a2f357d9003166f2a9838f1d8eda5c
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-11-15 01:13:39 +0330

    state and simple buttons

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Greet from "./components/Greet"
-import Welcome from "./components/welcome"
+import Counter from './components/Counter';
+// import Greet from "./components/Greet"
+// import Welcome from "./components/Welcome"
+import Message from "./components/Message"
 
 function App() {
   return (
     <div className="App">
+      <Message></Message>
+      <Counter></Counter>
       {/* <Greet name="Ali" heroName="DeadMeat"><p>This is children props</p></Greet>
       <Greet name="Mast" heroName="Caged"><button>Actions</button></Greet>
       <Greet name="Reza" heroName="Hmm"><ul><li>what</li></ul></Greet> */}
-      <Welcome name="Ali" heroName="DeadMeat"/>
+      {/* <Welcome name="Ali" heroName="DeadMeat"/>
       <Welcome name="Mast" heroName="Caged"/>
-      <Welcome name="Reza" heroName="Hmm"/>
+      <Welcome name="Reza" heroName="Hmm"/> */}
     </div>
   );
 }
