103855334cd076a458785f3cadb41b63390633dd
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-02 02:08:41 +0330

    Fragments

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,47 +1,16 @@
-// import logo from './logo.svg';
-import './App.css';
-// import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-// import Inline from './components/Inline';
-// import NameList from './components/NameList';
-// import EventBind from './components/EventBind';
-// import ParentComponent from './components/ParentComponent';
-// import UserGreetings from './components/UserGreetings';
-// import ClassClick from './components/ClassClick';
-// import FunctionClick from './components/FunctionClick';
-// import Counter from './components/Counter';
-// import Greet from "./components/Greet"
-// import Welcome from "./components/Welcome"
-// import Message from "./components/Message"
-// import StyleSheet from './components/StyleSheet';
-// import './AppStyles.css'
-// import Styles from './AppStyles.module.css'
-
-function App() {
-  return (
-    <div className="App">
-      {/* <StyleSheet primary={true}></StyleSheet> */}
-      {/* <h2 className='error'>Error</h2> */}
-      {/* <h2 className={Styles.success}>success</h2> */}
-      {/* <Message></Message> */}
-      {/* <Counter></Counter> */}
-      {/* <Greet name="Ali" heroName="DeadMeat"><p>This is children props</p></Greet> */}
-      {/* <Greet name="Mast" heroName="Caged"><button>Actions</button></Greet> */}
-      {/* <Greet name="Reza" heroName="Hmm"></Greet> */}
-      {/* <FunctionClick /> */}
-      {/* <Welcome name="Ali" heroName="DeadMeat" /> */}
-      {/* <Welcome name="Mast" heroName="Caged" /> */}
-      {/* <Welcome name="Reza" heroName="Hmm" /> */}
-      {/* <ClassClick></ClassClick> */}
-      {/* <EventBind></EventBind> */}
-      {/* <ParentComponent></ParentComponent> */}
-      {/* <UserGreetings></UserGreetings> */}
-      {/* <NameList></NameList> */}
-      {/* <Inline></Inline> */}
-      {/* <Form></Form> */}
-      <LifeCycleA></LifeCycleA>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
+export class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        {/* <FragmentDemo /> */}
+        <Table />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ hello-world/src/components/Columns.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Columns() {
+    return (
+        <React.Fragment>
+            <td>Name</td>
+            <td>Amin</td>
+        </React.Fragment>
+        // <>
+        //     <td>Name</td>
+        //     <td>Amin</td>
+        // </>
+    )
+}
+
+export default Columns
