867e364b8dea8b44fe5b69ee967c8084db33e262
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-02 20:28:56 +0330

    memo

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import './App.css'
 import ParentComp from './components/ParentComp'
+import './App.css'
 
 export class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ParentComp />
+        <ParentComp></ParentComp>
       </div>
     )
   }
