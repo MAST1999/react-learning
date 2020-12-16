09579bafafb7880c3e449265d6d07d38f6bc6fc9
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-16 19:08:53 +0330

    Render Props, you can use childern as well

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import './App.css'
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import Counter from './components/Counter'
+import HoverCounterTwo from './components/HoverCounterTwo'
+import User from './components/User'
 
 export class App extends Component {
   render() {
     return (
       <div className='App'>
-        <HoverCounter name='Amin' />
-        <ClickCounter />
+        {/* <HoverCounterTwo />
+        <ClickCounterTwo />
+        <User render={(isLoggedIN) => isLoggedIN ? 'Amin' : 'Guest'} /> */}
+        <Counter
+          render={(count, incrementCounter) => (<ClickCounterTwo count={count} incrementCounter={incrementCounter} />)}
+        />
+        <Counter render={(count, incrementCounter) => (<HoverCounterTwo count={count} incrementCounter={incrementCounter} />)} />
       </div>
     )
   }
