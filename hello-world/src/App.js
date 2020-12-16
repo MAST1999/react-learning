78af706caeedd1eb5c3064968daa46f9609ed769
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-16 22:51:16 +0330

    Context

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
-import ClickCounterTwo from './components/ClickCounterTwo'
-import Counter from './components/Counter'
-import HoverCounterTwo from './components/HoverCounterTwo'
-import User from './components/User'
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/UserContext'
+
 
 export class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <HoverCounterTwo />
-        <ClickCounterTwo />
-        <User render={(isLoggedIN) => isLoggedIN ? 'Amin' : 'Guest'} /> */}
-        <Counter
-          render={(count, incrementCounter) => (<ClickCounterTwo count={count} incrementCounter={incrementCounter} />)}
-        />
-        <Counter render={(count, incrementCounter) => (<HoverCounterTwo count={count} incrementCounter={incrementCounter} />)} />
+        <UserProvider value='Amin'>
+          <ComponentC />
+        </UserProvider>
       </div>
     )
   }
