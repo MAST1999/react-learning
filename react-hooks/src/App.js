48285d740a00059091cf35c0441d21d238a50d17
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-27 04:22:37 +0330

    useState with array

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css';
+import CounterFour from './components/CounterFour';
 import CounterThree from './components/CounterThree';
 import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <CounterThree />
+      <CounterFour />
     </div>
   );
 }
