8634610001b77bd3bc0dcce6b07791b74afae9b1
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-12-27 04:50:10 +0330

    Conditionally run effects

--- react-hooks/src/components/CounterOne.js
+++ react-hooks/src/components/CounterOne.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 
 function CounterOne() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('updating document title!');
         document.title = `Clicked ${count} times!`
-    })
+    }, [count])
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
@@ -14,6 +16,7 @@ function CounterOne() {
     return (
         <div>
             <button onClick={incrementCount}>Clicked {count} times!</button>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
         </div>
     )
 }
